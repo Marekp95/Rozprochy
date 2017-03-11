c97f610ec118664611576cfbc3182b58c0e72791
Marekp95 <marek-313-95@o2.pl>
2017-03-11 18:14:19 +0100

    lab1 ex2 and ex3

--- lab1/src/Client.java
+++ lab1/src/Client.java
@@ -11,37 +11,92 @@ public class Client {
 
         DatagramSocket datagramSocket = new DatagramSocket(socket.getLocalPort());
 
-   /*     openUDPListener(datagramSocket);
-        openMultiCast();*/
-
-        System.out.println("Enter nick:");
+        System.out.println("Insert your name:");
         Scanner scanner = new Scanner(System.in);
-        String nick = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-
-        printWriter.write(nick + '\n');
-        printWriter.flush();
-
-        Scanner finalScanner = new Scanner(socket.getInputStream());
+        //TCP communication
+        Scanner incomingMessages = new Scanner(socket.getInputStream());
         new Thread(() -> {
-            while (finalScanner.hasNextLine()) {
-                System.out.println("TCP " + finalScanner.nextLine());
+            while (incomingMessages.hasNextLine()) {
+                System.out.println("TCP message: " + incomingMessages.nextLine());
             }
 
         }).start();
+
+        //UDP communication
+        new Thread(() -> {
+            try {
+                while (true) {
+                    byte[] buff = new byte[Server.MSG_SIZE];
+                    DatagramPacket datagramPacket = new DatagramPacket(buff, buff.length);
+                    datagramSocket.receive(datagramPacket);
+                    String message = new String(buff);
+                    System.out.println("UDP message: " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        //multicast communication
+        new Thread(() -> {
+            try {
+                MulticastSocket multicastSocket = new MulticastSocket(Server.PORT_M);
+                multicastSocket.joinGroup(InetAddress.getByName(Server.IP_M));
+                while (true) {
+                    byte[] buff = new byte[Server.MSG_SIZE];
+                    DatagramPacket datagramPacket = new DatagramPacket(buff, buff.length);
+                    multicastSocket.receive(datagramPacket);
+                    String message = new String(buff);
+                    System.out.println("Multicast message: " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+
+        printWriter.write(name + '\n');
+        printWriter.flush();
 
         while (scanner.hasNextLine()) {
             String message = scanner.nextLine();
             if (message.equals("M") || message.equals("N")) {
                 InetAddress IPAddress = InetAddress.getByName("localhost");
 
-                byte[] bytes = (nick + ":" + message + ":                    \n" +
-                        "                 _.-;;-._\n" +
-                        "          '-..-'|   ||   |\n" +
-                        "          '-..-'|_.-;;-._|\n" +
-                        "          '-..-'|   ||   |\n" +
-                        "    jgs   '-..-'|_.-''-._|").getBytes();
+                byte[] bytes = (name + ";" + message + ";" + "\n" +
+                        "              .@.                                    .\n" +
+                        "              @m@,.                                 .@\n" +
+                        "             .@m%nm@,.                            .@m@\n" +
+                        "            .@nvv%vnmm@,.                      .@mn%n@\n" +
+                        "           .@mnvvv%vvnnmm@,.                .@mmnv%vn@,\n" +
+                        "           @mmnnvvv%vvvvvnnmm@,.        .@mmnnvvv%vvnm@\n" +
+                        "           @mmnnvvvvv%vvvvvvnnmm@, ;;;@mmnnvvvvv%vvvnm@,\n" +
+                        "           `@mmnnvvvvvv%vvvvvnnmmm;;@mmnnvvvvvv%vvvvnmm@\n" +
+                        "            `@mmmnnvvvvvv%vvvnnmmm;%mmnnvvvvvv%vvvvnnmm@\n" +
+                        "              `@m%v%v%v%v%v;%;%;%;%;%;%;%%%vv%vvvvnnnmm@\n" +
+                        "              .,mm@@@@@mm%;;@@m@m@@m@@m@mm;;%%vvvnnnmm@;@,.\n" +
+                        "           .,@mmmmmmvv%%;;@@vmvvvvvvvvvmvm@@;;%%vvnnm@;%mmm@,\n" +
+                        "        .,@mmnnvvvvv%%;;@@vvvvv%%%%%%%vvvvmm@@;;%%mm@;%%nnnnm@,\n" +
+                        "     .,@mnnvv%v%v%v%%;;@mmvvvv%%;*;*;%%vvvvmmm@;;%m;%%v%v%v%vmm@,.\n" +
+                        " ,@mnnvv%v%v%v%v%v%v%;;@@vvvv%%;*;*;*;%%vvvvm@@;;m%%%v%v%v%v%v%vnnm@,\n" +
+                        " `    `@mnnvv%v%v%v%%;;@mvvvvv%%;;*;;%%vvvmmmm@;;%m;%%v%v%v%vmm@'   '\n" +
+                        "         `@mmnnvvvvv%%;;@@mvvvv%%%%%%%vvvvmm@@;;%%mm@;%%nnnnm@'\n" +
+                        "            `@mmmmmmvv%%;;@@mvvvvvvvvvvmmm@@;;%%mmnmm@;%mmm@'\n" +
+                        "               `mm@@@@@mm%;;@m@@m@m@m@@m@@;;%%vvvvvnmm@;@'\n" +
+                        "              ,@m%v%v%v%v%v;%;%;%;%;%;%;%;%vv%vvvvvnnmm@\n" +
+                        "            .@mmnnvvvvvvv%vvvvnnmm%mmnnvvvvvvv%vvvvnnmm@\n" +
+                        "           .@mmnnvvvvvv%vvvvvvnnmm'`@mmnnvvvvvv%vvvnnmm@\n" +
+                        "           @mmnnvvvvv%vvvvvvnnmm@':%::`@mmnnvvvv%vvvnm@'\n" +
+                        "           @mmnnvvv%vvvvvnnmm@'`:::%%:::'`@mmnnvv%vvmm@\n" +
+                        "           `@mnvvv%vvnnmm@'     `:;%%;:'     `@mvv%vm@'\n" +
+                        "            `@mnv%vnnm@'          `;%;'         `@n%n@\n" +
+                        "             `@m%mm@'              ;%;.           `@m@\n" +
+                        "              @m@'                 `;%;             `@\n" +
+                        "              `@'                   ;%;.             '\n" +
+                        "               `                    `;%;             \n").getBytes();
                 datagramSocket.send(new DatagramPacket(bytes, bytes.length, IPAddress, Server.PORT));
             } else {
                 printWriter.write(message + '\n');
--- lab1/src/Server.java
+++ lab1/src/Server.java
@@ -1,10 +1,18 @@
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
     public static final int PORT = 12311;
+    public static final int PORT_M = 33011;
+    public static final String IP_M = "224.3.5.7";
+    public static final int MSG_SIZE = 4096;
+
+
     private Map<String, ServerThread> users = new HashMap<>();
 
     private void start() {
@@ -29,6 +37,61 @@ public class Server {
         }).start();
     }
 
+    private void startUDP() {
+        new Thread(() -> {
+            try {
+                DatagramSocket datagramSocket = new DatagramSocket(PORT);
+                while (!datagramSocket.isClosed()) {
+                    byte[] bytes = new byte[MSG_SIZE];
+                    DatagramPacket datagramPacket = new DatagramPacket(bytes, bytes.length);
+                    datagramSocket.receive(datagramPacket);
+                    String message = new String(bytes);
+
+                    String[] split = message.split(";", 3);
+
+                    if (split.length < 3) {
+                        continue;
+                    }
+
+                    String name = split[0];
+                    String messageType = split[1];
+                    message = split[2];
+
+                    if (messageType.equals("M") || messageType.equals("N")) {
+                        for (String userName: users.keySet()) {
+                            if(!userName.equals(name)){
+                                try {
+                                    int port;
+                                    String IP;
+                                    switch (messageType) {
+                                        case "M":
+                                            IP = "localhost";
+                                            port = users.get(userName).getSocket().getPort();
+                                            break;
+                                        case "N":
+                                            IP = IP_M;
+                                            port = PORT_M;
+                                            break;
+                                        default:
+                                            continue;
+                                    }
+                                    InetAddress IPAddress = InetAddress.getByName(IP);
+                                    byte[] newBytes = (name + ":" + message).getBytes();
+                                    datagramSocket.send(new DatagramPacket(newBytes, newBytes.length, IPAddress, port));
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+
     public void register(String name, ServerThread serverThread) {
         System.out.println("registered " + name);
         users.put(name, serverThread);
@@ -43,6 +106,8 @@ public class Server {
     }
 
     public static void main(String[] args) {
-        (new Server()).start();
+        Server server= new Server();
+        server.startUDP();
+        server.start();
     }
 }
